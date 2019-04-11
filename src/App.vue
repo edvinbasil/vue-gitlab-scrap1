b660a61fc1ce280b5db74b9374118ad45c2638aa
edvinbasil <edvinbasil@gmail.com>
2019-04-11 21:13:14 +0530

    Add basic sidebar and moved user info
    - Lint all files

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,33 @@
 <template>
   <div id="app">
-    <h1>{{ user.name }}</h1>
-    <p>{{ user.desc }}</p>
-    <h2>My Projects</h2>
+    <el-container style="height: 500px; border: 1px solid #eee">
+      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+        <h1>{{ user.name }}</h1>
+        <p>{{ user.desc }}</p>
+      </el-aside>
 
-    <el-table :data="projects" style="width: 75%; margin: auto">
-      <el-table-column prop="name" label="Name"></el-table-column>
-      <el-table-column prop="description" label="Description"></el-table-column>
-      <el-table-column prop="http_url_to_repo" label="URL">
-        <template slot-scope="scope">
-          <a
-            :href="scope.row.http_url_to_repo"
-            style="text-decoration:none; color: #356ca0"
-          >{{ scope.row.http_url_to_repo }}</a>
-        </template>
-      </el-table-column>
-    </el-table>
+      <el-container>
+        <el-main>
+          <h2>My Projects</h2>
+          <el-table :data="projects" style="width: 75%; margin: auto">
+            <el-table-column prop="name" label="Name"></el-table-column>
+            <el-table-column
+              prop="description"
+              label="Description"
+            ></el-table-column>
+            <el-table-column prop="http_url_to_repo" label="URL">
+              <template slot-scope="scope">
+                <a
+                  :href="scope.row.http_url_to_repo"
+                  style="text-decoration:none; color: #356ca0"
+                  >{{ scope.row.http_url_to_repo }}</a
+                >
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -42,8 +54,7 @@ export default {
             desc: data.bio,
             username: data.username
           };
-        })
-        
+        });
     },
     getProjects() {
       fetch(`https://gitlab.com/api/v4/users/${this.user_id}/projects`)
@@ -67,6 +78,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,14 +1,12 @@
 <template>
-  <div>
-  </div>
+  <div></div>
 </template>
 
 <script>
 export default {
-  name: "Project",
+  name: "Project"
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-</style>
+<style scoped></style>
